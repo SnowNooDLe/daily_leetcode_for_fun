fff9925b60cf81b5521dc2bdc348f47780f8cf99
Tom son <sdw123123@gmail.com>
2022-01-07 00:08:16 +1300

    Update slightly better solution with explanation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Daily Leetcode for Fun
+
+New year & New me
+This public repo is mainly for fun and motivating me to study more.
+
+I will try to solve&commit a question every day, starting from the easiest to the hardest.
+
--- questions/1_two_sum.py
+++ questions/1_two_sum.py
@@ -23,7 +23,7 @@ from typing import List
 
 def two_sum(nums: List[int], target: int):
     for first_index, first_value in enumerate(nums):
-        for second_index, second_value in enumerate(nums[first_index + 1 :]):
+        for second_index, second_value in enumerate(nums[first_index + 1:]):
             if first_value + second_value == target:
                 return [first_index, first_index + second_index + 1]
 
@@ -34,9 +34,34 @@ print(two_sum([3, 3], 6))
 
 
 """Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
-
 To achieve this, I guess we have to remove one of the for loops
 """
 
-# def two_sum_2(nums: List[int], target: int):
 
+def two_sum_2(nums: List[int], target: int):
+    """As specified, each input would have EXACTLY ONE SOLUTION and not use the SAME ELEMENT TWICE
+    Hence, start putting the difference between target and each value from the given list
+
+    For instance, with the first example:
+    The first value is 2 but 2 is not in the vals, add 9-2 = 7, now vals = [7]
+    The second value is 7 and 7 is in vals. Hence, we pair the index of current value from vals and nums
+    vals = [7], vals.index(value=7) = 0 and index which is 1 as it's the second element of the nums
+    as we go over the nums list, we create vals which is a temporary list, so the indexes are matching.
+
+    With the second example:
+    The first value is 3 but 3 is not in the vals, add 6-3=3, now vals = [3]
+    The second value is 2 but 2 is also not in the vals, add 6-2=4, now vals = [3,4]
+    The third value is 4 and 4 is in the vals, then we return as
+    vals = [3,4], vals.index(value=4) = 1 and index which is 2 => [1,2]
+
+    """
+    vals = []
+    for index, value in enumerate(nums):
+        if value in vals:
+            return [vals.index(value), index]
+        vals.append(target - value)
+
+
+print(two_sum_2([2, 7, 11, 15], 9))
+print(two_sum_2([3, 2, 4], 6))
+print(two_sum_2([3, 3], 6))
