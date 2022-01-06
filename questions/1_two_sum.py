96a5a741890146604c586ddf8e7e8e00008bcdd1
Tom son <sdw123123@gmail.com>
2022-01-06 23:31:35 +1300

    two_sum first solution

---
+++ questions/1_two_sum.py
@@ -0,0 +1,42 @@
+"""1. Two Sum
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+Example 1.
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2.
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3.
+Input: nums = [3,3], target = 6
+Output: [0,1]
+"""
+
+from typing import List
+
+
+def two_sum(nums: List[int], target: int):
+    for first_index, first_value in enumerate(nums):
+        for second_index, second_value in enumerate(nums[first_index + 1 :]):
+            if first_value + second_value == target:
+                return [first_index, first_index + second_index + 1]
+
+
+print(two_sum([2, 7, 11, 15], 9))
+print(two_sum([3, 2, 4], 6))
+print(two_sum([3, 3], 6))
+
+
+"""Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+To achieve this, I guess we have to remove one of the for loops
+"""
+
+# def two_sum_2(nums: List[int], target: int):
+
