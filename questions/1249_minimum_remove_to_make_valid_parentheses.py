a9944742e8d9fe123a90fa0c317cfe27ad027e0c
Tom son <sdw123123@gmail.com>
2022-03-15 23:03:03 +1300

    Daily code 15 - updated version

--- questions/1249_minimum_remove_to_make_valid_parentheses.py
+++ questions/1249_minimum_remove_to_make_valid_parentheses.py
@@ -28,43 +28,37 @@ Explanation: An empty string is also valid.
 
 
 def minRemoveToMakeValid(s: str) -> str:
-    """Runtime 161ms, Memory 49.89"""
+    """Runtime 133ms, Memory 16MB"""
     trace_stack = []
-    answer = []
+    s = list(s)
     for idx, char in enumerate(s):
         # We keep tracking the open parentheses as the valid parentheses look like ()
         if char == "(":
             # We need to have index for ( so we can potentially remove if they are not a valid parentheses
             trace_stack.append({"index": idx, "value": char})
-            answer.append(char)
         elif char == ")":
             # Never visited ( but ) came, starting with ) is useless so we store "" instead
             if len(trace_stack) == 0:
                 # string += "" does not do anything but we still need to reserve the string at a certain index
                 # even thought these will be removed
-                answer.append("")
+                # Updated version - We now use list so we can replace it
+                s[idx] = ""
             else:
                 # At least we have something in stack(either ( or )), and if the last one is ( remove it
                 # and we can safely add ) as it's a valid parentheses
                 if trace_stack[-1]["value"] == "(":
                     trace_stack.pop()
-                    answer.append(char)
-        # Other than parentheses, keep adding
-        else:
-            answer.append(char)
 
-    # Convert to list due to an error
-    # 'str' object does not support item assignment
-    answer = list(answer)
     # Any left open parentheses, as they are not valid, remove them.
     for stack in trace_stack:
-        answer[stack["index"]] = ""
+        s[stack["index"]] = ""
 
     # Because answer is currently in a list, join them and remove any empty string
-    return "".join(answer)
+    return "".join(s)
 
 
 print(minRemoveToMakeValid("lee(t(c)o)de)"))
 print(minRemoveToMakeValid("a)b(c)d"))
 print(minRemoveToMakeValid("))(("))
 print(minRemoveToMakeValid("(a(b(c)d)"))
+print(minRemoveToMakeValid("()()"))
